04bc25fc8a53f89f1faa4344bf9863832f8003ab
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2021-05-20 10:55:50 -0300

    Usuário digita o ticker.
    Inserida a funcionalidade do usuário digitar o Ticker que ele desejar.

--- Meus testes/PandasDataReader.py
+++ Meus testes/PandasDataReader.py
@@ -7,17 +7,26 @@ expire_after = datetime.timedelta(days=3)
 session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=expire_after)
 start = datetime.datetime(2021, 5, 1)
 end = datetime.datetime(2021, 5, 19)
-guar3 = web.DataReader("GUAR3.SA", 'yahoo', start, end, session=session)
-hype3 = web.DataReader("HYPE3.SA", 'yahoo', start, end, session=session)
-itub4 = web.DataReader("ITUB4.SA", 'yahoo', start, end, session=session)
-bbas3 = web.DataReader("BBAS3.SA", 'yahoo', start, end, session=session)
+ticker_usuario = input("Digite o código da ação: ")
+ticker_usuario = ticker_usuario + ".SA"             # Para ações do IBOV é necessário add '.SA'.
+ticker = ticker_usuario.upper()                     # Converte tudo em maiúscula. 
 
+# Recebe os dados e armazena na variável papel. 
+papel = web.DataReader(ticker, 'yahoo', start, end, session=session)
+papel = round(papel, 2) # Arredonda para 2 casas decimais.
 
-#Testes
-print(guar3.loc['2021-5-19'])
-guar3_vol = guar3.Volume
-print(f'O volume de Guar3 foi\n{guar3_vol}')
+# Mostra as informações sobre a tabela, nomes de colunas e tipos de variáveis.
+print(papel.info())
 
-#print(len(guar3_vol))
-#print(itub4.all)
-#print(itub4.head(6))
+print(
+f'As 5 primeiras linhas da tabela:\n'
+f'{papel.head()}'
+)
+
+print(
+f'As informações de determinado dia:\n'
+f'{papel.loc["2021-5-19"]}'
+)
+
+papel_vol = papel.Volume
+print(f'O volume de {ticker} foi\n{papel_vol}')
